92e221fd3d523297de368d8d735a2768dec5b826
Zain-firoz <zainfiroz007@gmail.com>
2024-06-15 13:10:30 +0530

    first commit

---
+++ apps/client/src/pages/signup.tsx
@@ -0,0 +1,7 @@
+import {Signup} from "../../../../packages/ui/src/components/Signup"
+
+export default function SignupPage() {
+    return <div>
+        <Signup />
+    </div>
+}
---
+++ packages/ui/src/components/Signup.tsx
@@ -0,0 +1,7 @@
+export function Signup() {
+    return <div>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="password"></input>
+        <button>Submit</button>
+    </div>
+}
